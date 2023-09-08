e5f1694d58116e361802675bbc7d52b871409c86
Ayodele Fagbami <afagbami@gmail.com>
2023-09-08 17:45:50 +0100

    added react-router-dom to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Entries from "./components/Entries";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <main className="p-[10px] max-w-[700px] mx-auto my-0">
-      <Navbar />
-      <Entries />
-    </main>
+    <Routes>
+      <Route
+        index
+        element={
+          <main className="p-[10px] max-w-[700px] mx-auto my-0">
+            <Navbar />
+            <Entries />
+          </main>
+        }
+      />
+    </Routes>
   );
 }
 
