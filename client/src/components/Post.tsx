e627d15549a044d9417be2e7e83681327e25dd5a
Ayodele Fagbami <afagbami@gmail.com>
2023-09-08 17:41:37 +0100

    extracted post to separate component

---
+++ client/src/components/Post.tsx
@@ -0,0 +1,32 @@
+const Post = () => {
+  return (
+    <div className="grid grid-cols-[0.9fr,1.1fr] gap-[20px] mb-[30px]">
+      <div className="">
+        <img
+          className="object-cover h-[100%]"
+          src="https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1360/1200x800/filters:focal(1020x680:1021x681):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/22173805/DSCF2666_2.jpg"
+          alt="Airpods Img"
+        />
+      </div>
+      <div>
+        <h2 className="m-0 text-[1.5rem] font-bold">
+          Apple’s AirPods and AirPods Max could get USB-C ports next year
+        </h2>
+        <p className="mx-0 my-[6px] text-[#888] text-[.7rem] font-bold flex gap-[10px]">
+          <a href="" className="text-[#333]">
+            Ayodele Fagbami
+          </a>
+          <time>2023-09-06 17:19</time>
+        </p>
+        <p className="text-sm my-[10px] mx-0 leading-[1.4rem]">
+          Apple is expected to switch its AirPods Pro earbuds to a USB-C
+          charging case during next week’s iPhone 15 event. And now, Bloomberg’s
+          Mark Gurman has revealed when the rest of the AirPods lineup will
+          follow suit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
