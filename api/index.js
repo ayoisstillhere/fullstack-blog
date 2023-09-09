41514903badf3b29b092c09ded3f403533549d6c
Ayodele Fagbami <afagbami@gmail.com>
2023-09-09 16:41:42 +0100

    connect to mongoDB using URI

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+dbURI =
+  "mongodb+srv://afagbami:qcssRq52gt22tuOS@cluster0.djv2j68.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
 
 app.post("/register", (req, res) => {
   const { username, password } = req.body;
