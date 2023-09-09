058cc5c96def040ce5cb34ccc54fe5a6f54563bf
Ayodele Fagbami <afagbami@gmail.com>
2023-09-09 17:22:54 +0100

    encrypting password

--- api/index.js
+++ api/index.js
@@ -3,7 +3,10 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const dbURI = require("./constants");
+const bycrypt = require("bcryptjs");
 const app = express();
+
+const salt = bycrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -13,7 +16,10 @@ mongoose.connect(dbURI);
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({
+      username,
+      password: bycrypt.hashSync(password, salt),
+    });
     res.json(userDoc);
   } catch (error) {
     res.status(400).json({ message: error.message });
