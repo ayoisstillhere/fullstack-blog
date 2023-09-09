93fde8f36c43bebce27575ac71ac183ee7cb2f5a
Ayodele Fagbami <afagbami@gmail.com>
2023-09-09 17:56:16 +0100

    added include so cookie will be in next request

--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -9,6 +9,7 @@ const LoginPage = () => {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
+      credentials: "include",
     });
   }
   return (
