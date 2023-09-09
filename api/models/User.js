7f28dde3e53f821e00eb4a9a5235536c870e0ab4
Ayodele Fagbami <afagbami@gmail.com>
2023-09-09 16:36:24 +0100

    created user model

---
+++ api/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, min: 4, unique: true },
+  password: { type: String, required: true },
+});
+
+const UserModel = model("User", UserSchema);
+module.exports = UserModel;
