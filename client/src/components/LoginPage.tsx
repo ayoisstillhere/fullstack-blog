7e897433e7c8f08de878261a37df7fe79f4fbae8
Ayodele Fagbami <afagbami@gmail.com>
2023-09-09 18:05:21 +0100

    redirect to homepage on successful login

--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
   async function login(ev: { preventDefault: () => void }) {
     ev.preventDefault();
-    await fetch("http://localhost:4000/login", {
+    const response = await fetch("http://localhost:4000/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
       credentials: "include",
     });
+    if (response.ok) {
+      setRedirect(true);
+    } else {
+      alert("Wrong Credentials");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to="/" />;
   }
   return (
     <form className="max-w-[400px] mx-auto my-0" onSubmit={login}>
