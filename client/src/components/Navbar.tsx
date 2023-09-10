83abeab2cf51529b9c745fa30c0d405d248f7bfd
Ayodele Fagbami <afagbami@gmail.com>
2023-09-10 13:05:39 +0100

    fetch request for token in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", { credentials: "include" });
+  }, []);
   return (
     <header className="flex justify-between mb-[50px] items-center mt-[20px]">
       <Link to="/" className="font-bold text-[1.8rem]">
